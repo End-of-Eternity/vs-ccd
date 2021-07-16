811318623795dcaadf839e2c7750a9506852eee2
Arjun Raj <arjr4j@gmail.com>
2021-07-16 11:41:55 +0100

    as before, but in float instead of int

--- src/ccd.cpp
+++ src/ccd.cpp
@@ -7,7 +7,7 @@
 #include <VSHelper.h>
 
 // i dont know if there's a better way to do this :dek:
-static const double DIVISORS[] = {1., 1. / 2, 1. / 3, 1. / 4, 1. / 5, 1. / 6, 1. / 7, 1. / 8, 1. / 9, 1. / 10, 1. / 11, 1. / 12, 1. / 13, 1. / 14, 1. / 15, 1. / 16, 1. / 17, 1. / 18, 1. / 19, 1. / 20};
+static const float DIVISORS[] = {1., 1. / 2, 1. / 3, 1. / 4, 1. / 5, 1. / 6, 1. / 7, 1. / 8, 1. / 9, 1. / 10, 1. / 11, 1. / 12, 1. / 13, 1. / 14, 1. / 15, 1. / 16, 1. / 17, 1. / 18, 1. / 19, 1. / 20};
 
 typedef struct ccdData
 {
@@ -21,13 +21,13 @@ static void ccd_run(const VSFrameRef *src, VSFrameRef *dest, float threshold, co
     int width = vsapi->getFrameWidth(src, 0);
     int height = vsapi->getFrameHeight(src, 0);
 
-    auto *src_r_plane = reinterpret_cast<const uint8_t *>(vsapi->getReadPtr(src, 0));
-    auto *src_g_plane = reinterpret_cast<const uint8_t *>(vsapi->getReadPtr(src, 1));
-    auto *src_b_plane = reinterpret_cast<const uint8_t *>(vsapi->getReadPtr(src, 2));
+    auto *src_r_plane = reinterpret_cast<const float *>(vsapi->getReadPtr(src, 0));
+    auto *src_g_plane = reinterpret_cast<const float *>(vsapi->getReadPtr(src, 1));
+    auto *src_b_plane = reinterpret_cast<const float *>(vsapi->getReadPtr(src, 2));
 
-    auto *dst_r_plane = reinterpret_cast<uint8_t *>(vsapi->getWritePtr(dest, 0));
-    auto *dst_g_plane = reinterpret_cast<uint8_t *>(vsapi->getWritePtr(dest, 1));
-    auto *dst_b_plane = reinterpret_cast<uint8_t *>(vsapi->getWritePtr(dest, 2));
+    auto *dst_r_plane = reinterpret_cast<float *>(vsapi->getWritePtr(dest, 0));
+    auto *dst_g_plane = reinterpret_cast<float *>(vsapi->getWritePtr(dest, 1));
+    auto *dst_b_plane = reinterpret_cast<float *>(vsapi->getWritePtr(dest, 2));
 
     for (int y = 0; y < height; y++)
     {
@@ -38,12 +38,9 @@ static void ccd_run(const VSFrameRef *src, VSFrameRef *dest, float threshold, co
         {
             int i = y * width + x;
 
-            auto r = src_r_plane[i];
-            auto g = src_g_plane[i];
-            auto b = src_b_plane[i];
-
-            int total_r = r, total_g = g, total_b = b;
-            int n = 0;
+            float r = src_r_plane[i], g = src_g_plane[i], b = src_b_plane[i];
+            float total_r = r, total_g = g, total_b = b;
+            int n = 1;
 
             int x_start = x > 11 ? x - 12 : x;
             int x_end = x < width - 12 ? x + 12 : x;
@@ -53,13 +50,13 @@ static void ccd_run(const VSFrameRef *src, VSFrameRef *dest, float threshold, co
                 int y_offset = comp_y * width;
                 for (int comp_x = x_start; comp_x < x_end; comp_x += 8)
                 {
-                    auto comp_r = src_r_plane[y_offset + comp_x];
-                    auto comp_g = src_g_plane[y_offset + comp_x];
-                    auto comp_b = src_b_plane[y_offset + comp_x];
+                    float comp_r = src_r_plane[y_offset + comp_x];
+                    float comp_g = src_g_plane[y_offset + comp_x];
+                    float comp_b = src_b_plane[y_offset + comp_x];
 
-                    auto diff_r = comp_r - r;
-                    auto diff_g = comp_g - r;
-                    auto diff_b = comp_b - r;
+                    float diff_r = comp_r - r;
+                    float diff_g = comp_g - r;
+                    float diff_b = comp_b - r;
 
 #define SQUARE(x) (x * x)
                     if (threshold > (SQUARE(diff_r) + SQUARE(diff_g) + SQUARE(diff_b)))
@@ -73,28 +70,28 @@ static void ccd_run(const VSFrameRef *src, VSFrameRef *dest, float threshold, co
                 }
             }
 
-            auto calculated_r = total_r * DIVISORS[n];
-            auto calculated_g = total_g * DIVISORS[n];
-            auto calculated_b = total_b * DIVISORS[n];
+            float calculated_r = total_r * DIVISORS[n];
+            float calculated_g = total_g * DIVISORS[n];
+            float calculated_b = total_b * DIVISORS[n];
 
             if (calculated_r < 0)
                 calculated_r = 0;
-            else if (calculated_r > 255)
-                calculated_r = 255;
+            else if (calculated_r > 1)
+                calculated_r = 1;
 
             if (calculated_g < 0)
                 calculated_g = 0;
-            else if (calculated_g > 255)
-                calculated_g = 255;
+            else if (calculated_g > 1)
+                calculated_g = 1;
 
             if (calculated_b < 0)
                 calculated_b = 0;
-            else if (calculated_b > 255)
-                calculated_b = 255;
+            else if (calculated_b > 1)
+                calculated_b = 1;
 
-            dst_r_plane[i] = calculated_r + 0.5;
-            dst_g_plane[i] = calculated_g + 0.5;
-            dst_b_plane[i] = calculated_b + 0.5;
+            dst_r_plane[i] = calculated_r;
+            dst_g_plane[i] = calculated_g;
+            dst_b_plane[i] = calculated_b;
         }
     }
 }
@@ -168,9 +165,9 @@ static void VS_CC ccdCreate(const VSMap *in, VSMap *out, void *userData, VSCore 
         vsapi->freeNode(d.node);
     }
 
-    if (d.vi->format->id != 2000010)
+    if (d.vi->format->id != 2000015)
     { // ID of RGBS
-        vsapi->setError(out, "CCD: Input clip must be RGB24");
+        vsapi->setError(out, "CCD: Input clip must be RGBS");
         vsapi->freeNode(d.node);
     }
 
