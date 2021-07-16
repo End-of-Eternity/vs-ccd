c90d0c1752c84721b92a7ae70f4a2057c15ab47c
Scrad <60075611+ScradOWO@users.noreply.github.com>
2021-07-16 12:55:20 +0530

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/ccd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ccd.dir/ccd.cpp.obj"
+  "libccd.a"
+  "libccd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ccd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
